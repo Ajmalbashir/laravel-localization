931e35b151009222f35ce25d8b7104036f444848
Ajmalbashir <muhammadajmalbashir44@gmail.com>
2024-02-01 19:25:12 +0500

    changed the title of the app

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>flag-icon</title>
+  <title>Localization</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
