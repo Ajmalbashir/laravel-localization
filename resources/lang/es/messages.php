413c78734d2e1ab7db755617bf2afb07ce1047f3
Ajmalbashir <muhammadajmalbashir44@gmail.com>
2024-02-01 19:11:20 +0500

    first commit

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'welcome' => 'Bienvenidos a nuestra aplicación!',
+];
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>flag-icon</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown float-right">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="flag-icon flag-icon-{{ Config::get('languages')[App::getLocale()]['flag-icon'] }}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                @foreach (Config::get('languages') as $lang => $language)
+                    @if ($lang != App::getLocale())
+                        <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}">
+                            <span class="flag-icon flag-icon-{{ $language['flag-icon'] }}"></span> {{ $language['display'] }}
+                        </a>
+                    @endif
+                @endforeach
+            </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+      <div id="content" class="p-4 p-md-5 pt-5">
+        @yield('content')
+      </div>
+
+  <!-- Bootstrap JS (Assuming you're using Bootstrap) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
